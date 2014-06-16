084526bd133657c895b4d636f30bc941272f6406
Ciaran Downey <me@ciarand.me>
2014-06-16 10:08:24 -0700

    Add some potential topic ideas

--- app/views/present.blade.php
+++ app/views/present.blade.php
@@ -9,3 +9,57 @@ Getting started
 composer install
 php artisan serve
 ```
+
+Cool things
+-----------
+```php
+Response::markdown("viewname", ["data" => "goes here"]);
+```
+
+Possible topic ideas
+--------------------
+- Constructing a SQL query w/ joins
+
+- Comparing two dates
+
+- Doing template inheritance (i.e. not repeating `<!DOCTYPE>`)
+
+- Simple routing
+
+- Loading files and classes automatically
+
+- "Pretty" urls (without `.php`, GET params w/out `?`, etc.)
+
+- (Reversible) database migrations
+
+- Functional testing (simulating HTTP requests)
+
+- Simple CRUD for simple models
+
+- JSON responses (headers too)
+
+- Redirecting
+
+- CSRF tokens
+
+- Authentication (password hashing, encryption)
+
+- Deployment process
+
+- Repeatable environments (Vagrant)
+
+- Debug mode
+
+- i8n
+
+- Logging
+
+- Email
+
+- Queues
+
+- Making CURL requests
+
+- Maintenance mode
+
+- Error handling
