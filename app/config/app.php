15e3893648f1ee21455d81ac79f21dc896d62ebe
Ciaran Downey <me@ciarand.me>
2014-06-22 19:19:03 -0700

    Retab app/config/app.php

--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,7 @@ return array(
         'Illuminate\Validation\ValidationServiceProvider',
         'Illuminate\View\ViewServiceProvider',
         'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Way\Generators\GeneratorsServiceProvider',
+        'Way\Generators\GeneratorsServiceProvider',
 
     ),
 
@@ -189,8 +189,7 @@ return array(
         'URL'             => 'Illuminate\Support\Facades\URL',
         'Validator'       => 'Illuminate\Support\Facades\Validator',
         'View'            => 'Illuminate\Support\Facades\View',
-		'Carbon'		  => 'Carbon\Carbon',
+        'Carbon'          => 'Carbon\Carbon',
 
     ),
-
 );
